a0adf67077ff1c03f09c846d0f68cfb2122fcc9a
sanjib <sanjibbarakoti112@gmail.com>
2024-10-19 17:45:01 -0400

    working blog frontend

--- src/CommentList.jsx
+++ src/CommentList.jsx
@@ -1,27 +1,22 @@
-//this will display list of comments on the Comment.
-//It will take postId as a props
-//
 
-import { useState, useEffect } from "react";
-import axios from "axios";
+const CommentList = ({comments}) => {
 
-const CommentList = ({postId}) => {
-   const [comments, setComments] = useState({});
+   const renderedComments = comments.map((comment) => {
+      let content;
 
-   const fetchComments = async () => {
-      const res = await axios.get(
-         `http://localhost:4001/posts/${postId}/comments`
-      );
+      if(comment.status === 'approved'){
+         content = comment.content;
+      }
 
-      setComments(res.data);
-   };
+      if(comment.status === 'pending'){
+         content = 'This comment is awaiting moderation';
+      }
 
-   useEffect(() => {
-      fetchComments();
-   }, []);
+      if(comment.status === 'rejected'){
+         content = 'This comment has been rejected';
+      }
 
-   const renderedComments = Object.values(comments).map((comment) => {
-      return <li key={comment.id}>{comment.content}</li>;
+      return <li key={comment.id}>{content}</li>;
    });
 
    return <ul>{renderedComments}</ul>;
--- src/PostCreate.jsx
+++ src/PostCreate.jsx
@@ -4,16 +4,33 @@ import axios from "axios";
 //component that handle the functionality of blogs posts.
 const PostCreate = () => {
    const [title, setTitle] = useState("");
-
+    
+   const [isLoading, setIsLoading] = useState(false);
+    
    const onSubmit = async (event) => {
       event.preventDefault();
+      console.log('one');
 
-      await axios.post("http://localhost:4000/posts", {
-         title,
-      });
 
-      setTitle("");
+      try {
+         setIsLoading(true);
+         await axios.post("http://localhost:4000/posts", {
+            title,
+         });
+         setIsLoading(false);
+         setTitle("");
+         
+      } catch (error) {
+         console.log(error);
+      }
+      console.log(two);
+
+
    };
+
+   if(isLoading) {
+      return "Loading ...."
+   }
 
    return (
       <div>
